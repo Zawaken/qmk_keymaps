08ccf666f0843b6decf649f8d96e096e47093b69
Zawaken <zawaken@outlook.com>
2020-02-25 13:45:17 +0100

    LWRSPC/RAISPC does not work the way I want it to

--- README.md
+++ README.md
@@ -5,3 +5,8 @@
 
 
 Flash script shamelessly stolen and modified from [ai212983](https://github.com/ai212983)
+
+
+## TODO:
+* Fix common Variables/find out why it is not currently working.
+* Lower/Raise on hold, space on tap, does not allow going to the ADJUST layer.
--- preonic/keymap.c
+++ preonic/keymap.c
@@ -39,8 +39,6 @@ enum preonic_keycodes {
 
 #define ARROW MO(_ARROW)
 #define CTL_ESC MT(MOD_LCTL, KC_ESC)
-#define LWRSPC LT(_LOWER, KC_SPC)
-#define RAISPC LT(_RAISE, KC_SPC)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -62,7 +60,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,  \
   CTL_ESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_SFTENT,  \
-  ARROW, KC_ALGR, KC_LALT, KC_LGUI, LWRSPC,   KC_SPC,  KC_SPC,  RAISPC,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT  \
+  ARROW, KC_ALGR, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_BSPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT  \
 ),
 
 /* Colemak
@@ -83,7 +81,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSPC,  \
   CTL_ESC,  KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT, \
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_SFTENT,  \
-  ARROW, KC_LCTL, KC_LALT, KC_LGUI, LWRSPC,   KC_SPC,  KC_SPC,  RAISPC,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT  \
+  ARROW, KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT  \
 ),
 
 /* Workman
@@ -104,7 +102,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,  KC_Q, KC_D, KC_R,  KC_W,    KC_B,    KC_J,    KC_F,    KC_U,    KC_P,    KC_SCLN,    KC_BSPC,  \
   CTL_ESC,  KC_A,    KC_S,    KC_H,    KC_T,    KC_G,    KC_Y,    KC_N,    KC_E,    KC_O,    KC_I,    KC_QUOT, \
   KC_LSFT, KC_Z, KC_X,    KC_M,    KC_C,    KC_V,    KC_K,    KC_L,    KC_COMM,    KC_DOT,    KC_SLSH,    KC_SFTENT,  \
-  ARROW, KC_LCTL, KC_LALT, KC_LGUI, LWRSPC,   KC_SPC,  KC_SPC,  RAISPC,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT  \
+  ARROW, KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT  \
 ),
 
 /* Arrow layout
