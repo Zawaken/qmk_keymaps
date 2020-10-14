8b83c29c97b78764543cda9089608fd3fe52e40f
Zawaken <zawaken@outlook.com>
2020-10-14 20:13:23 +0200

    updates to flash and nyquist/viterbi

--- flash.sh
+++ flash.sh
@@ -1,16 +1,21 @@
 #!/usr/bin/env bash
+
+
 show_help() {
-printf '    -h     display this help and exit
+printf '    -h     display this menu and exit
     -b     build only mode, no flashing
     -v     verbose mode
+    -p     build with podman instead of Docker
 
-Example: ./flash.sh -d -v -f planck_ez\n'
+Example: ./flash.sh -p -v preonic\n'
 }
 
 verbose=0
 build_only='false'
-
+QMK_FIRMWARE='qmk_firmware'
+QMK_USER='zawaken'
 eval LAST_ARG=\${$#}
+TARGET_LAYOUT=${LAST_ARG%/}
 
 while true; do
   case $1 in
@@ -21,11 +26,14 @@ while true; do
     -v|--verbose)
       verbose=1
       ;;
+    -u|--user)
+      QMK_USER="${2}"
+      ;;
     -b|--build_only)
       build_only=true
       ;;
     -p|--podman)
-      podman=1
+      PODMAN=1
       ;;
     *)
       break
@@ -37,10 +45,6 @@ done
 if test "${verbose}" -eq 1; then
   set -o xtrace
 fi
-
-QMK_FIRMWARE='qmk_firmware'
-QMK_USER='zawaken'
-TARGET_LAYOUT=${LAST_ARG%/}
 
 if test "${TARGET_LAYOUT}" = "planck"; then
   TARGET_KEYBOARD='planck'
@@ -77,23 +81,80 @@ else
   IMAGE_EXTENSION='hex'
 fi
 
+#if test -z "${TARGET_LAYOUT}"; then
+#  case "${TARGET_LAYOUT}" in
+#    planck)
+#      TARGET_KEYBOARD='planck'
+#      MAKE_PREFIX='planck/rev6'
+#      IMAGE_EXTENSION='bin'
+#      MAKE_SUFFIX=':dfu-util-wait'
+#      ;;
+#    preonic)
+#      TARGET_KEYBOARD='preonic'
+#      MAKE_PREFIX='preonic/rev3'
+#      IMAGE_EXTENSION='bin'
+#      MAKE_SUFFIX=":dfu-util-wait"
+#      ;;
+#    m60-a)
+#      TARGET_KEYBOARD='rama_works_m60_a'
+#      MAKE_PREFIX='wilba_tech/rama_works_m60_a'
+#      IMAGE_EXTENSION='hex'
+#      ;;
+#    viterbi)
+#      TARGET_KEYBOARD='keebio/viterbi'
+#      MAKE_PREFIX='keebio/viterbi/rev2'
+#      IMAGE_EXTENSION='hex'
+#      MAKE_SUFFIX=':dfu'
+#      ;;
+#    nyquist)
+#      TARGET_KEYBOARD='keebio/nyquist'
+#      MAKE_PREFIX='keebio/nyquist/rev3'
+#      IMAGE_EXTENSION='hex'
+#      MAKE_SUFFIX=':dfu'
+#      ;;
+#    vitamins_included)
+#      TARGET_KEYBOARD='vitamins_included'
+#      MAKE_PREFIX='vitamins_included/rev2'
+#      IMAGE_EXTENSION='hex'
+#      MAKE_SUFFIX=':dfu'
+#      ;;
+#    *)
+#      printf "${TARGET_LAYOUT} not found in the list, maybe you should add it?\nexiting"
+#      exit 1
+#      ;;
+#  esac
+#fi
+
 BINARY_NAME="${TARGET_LAYOUT}_${QMK_USER}.${IMAGE_EXTENSION}"
 
-printf "===================================================
+printf "\n===================================================
 Using firmware folder ${QMK_FIRMWARE}
 Layout: ${TARGET_LAYOUT}
 Building binary '${BINARY_NAME}'"
 
+# Remove .build folder
 rm -rf $QMK_FIRMWARE/.build
-printf "\n\nCopying keymap \"${MAKE_PREFIX}:${QMK_USER}\" \n\n"
+
+# Create TARGET_KEYBOARD/keymaps/QMK_USER folder
 mkdir -p $QMK_FIRMWARE/keyboards/$TARGET_KEYBOARD/keymaps/${QMK_USER}
+
+# Copy keymap files for the specified keymap
+printf "\n\nCopying keymap \"${MAKE_PREFIX}:${QMK_USER}\" \n\n"
 rsync -avh $1/{config.h,keymap.c,rules.mk} $QMK_FIRMWARE/keyboards/$TARGET_KEYBOARD/keymaps/${QMK_USER}/ # >/dev/null 2>&1
+
+# Copy everything in the common directory into users/QMK_USER
 printf "Copying ./common to \"./${QMK_FIRMWARE}/users/${QMK_USER}\"\n\n"
 rsync -avh --delete common/ $QMK_FIRMWARE/users/$QMK_USER/ >/dev/null 2>&1
+
+# Submodule initialization
 make --directory=qmk_firmware git-submodule >/dev/null 2>&1
+
+# Remove previous binary
 rm -rf $QMK_FIRMWARE/${TARGET_LAYOUT}_${QMK_USER}.${IMAGE_EXTENSION}
+
+# Build/flash
 cd $QMK_FIRMWARE
-if test $podman -eq 1; then
+if test ${PODMAN}; then
   rsync -avh --delete ../podman_build.sh ./util/
   if $build_only; then
     printf 'build_only specified, skipping flash\n===================================================\n\n'
@@ -112,4 +173,3 @@ else
     ./util/docker_build.sh ${MAKE_PREFIX}:${QMK_USER}${MAKE_SUFFIX}
   fi
 fi
-cd ..
