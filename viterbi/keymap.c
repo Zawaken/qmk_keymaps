5dbb3d27ba1fa4e2f87ea0cd6de35c7c20869d14
Zawaken <zawaken@outlook.com>
2021-07-03 18:21:42 +0200

    Sat Jul  3 06:21:42 PM CEST 2021

--- viterbi/keymap.c
+++ viterbi/keymap.c
@@ -12,8 +12,8 @@
 #define _ADJUST 16
 
 enum custom_keycodes {
-  QWERTY = SAFE_RANGE,
-  COLEMAK,
+  COLEMAK = SAFE_RANGE,
+  QWERTY,
   LOWER,
   RAISE,
   ADJUST,
@@ -22,27 +22,6 @@ enum custom_keycodes {
 #define CTL_ESC MT(MOD_LCTL, KC_ESC)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-
-/* QWERTY
- * ,------------------------------------------------.      ,------------------------------------------------.
- * |  Ins |   `  |   1  |   2  |   3  |   4  |   5  |      |   6  |   7  |   8  |   9  |   0  | Bksp | Del  |
- * |------+------+------+------+------+------+------|      |------+------+------+------+------+------+------|
- * |   -  | Tab  |   Q  |   W  |   E  |   R  |   T  |      |   Y  |   U  |   I  |   O  |   P  |   [  |   ]  |
- * |------+------+------+------+------+------+------|      |------+------+------+------+------+------+------|
- * |   =  | Esc  |   A  |   S  |   D  |   F  |   G  |      |   H  |   J  |   K  |   L  |   ;  |   "  | Home |
- * |------+------+------+------+------+------+------|      |------+------+------+------+------+------+------|
- * | Pg Up| Shift|   Z  |   X  |   C  |   V  |   B  |      |   N  |   M  |   ,  |   .  |   /  |Enter | End  |
- * |------+------+------+------+------+------+------|      |------+------+------+------+------+------+------|
- * | Pg Dn|Adjust| Ctrl | Alt  | GUI  |Lower |Space |      |Space |Raise | Left | Down |  Up  | Right|   \  |
- * `------------------------------------------------'      `------------------------------------------------'
- */
-  [_QWERTY] = LAYOUT_ortho_5x14(
-    KC_INS,  KC_GRV , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,     KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_BSLS, KC_LBRC,
-    KC_MINS, KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   ,     KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_BSPC, KC_RBRC,
-    KC_EQL,  CTL_ESC, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   ,     KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT, KC_HOME,
-    KC_PGUP, KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   ,     KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_ENT , KC_END ,
-    KC_PGDN, ADJUST , KC_LCTL, KC_LALT, KC_LGUI, LOWER  , KC_SPC ,     KC_BSPC , RAISE  , KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, KC_DEL
-  ),
 
 /* COLEMAK
  * ,------------------------------------------------.      ,------------------------------------------------.
@@ -63,6 +42,27 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_EQL,  CTL_ESC, KC_A   , KC_R   , KC_S   , KC_T   , KC_D   ,     KC_H   , KC_N   , KC_E   , KC_I   , KC_O   , KC_QUOT, KC_HOME,
     KC_PGUP, KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   ,     KC_K   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_ENT , KC_END ,
     KC_PGDN, ADJUST , KC_ALGR, KC_LALT, KC_LGUI, LOWER  , KC_SPC ,     KC_BSPC , RAISE  , KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, KC_END
+  ),
+
+/* QWERTY
+ * ,------------------------------------------------.      ,------------------------------------------------.
+ * |  Ins |   `  |   1  |   2  |   3  |   4  |   5  |      |   6  |   7  |   8  |   9  |   0  | Bksp | Del  |
+ * |------+------+------+------+------+------+------|      |------+------+------+------+------+------+------|
+ * |   -  | Tab  |   Q  |   W  |   E  |   R  |   T  |      |   Y  |   U  |   I  |   O  |   P  |   [  |   ]  |
+ * |------+------+------+------+------+------+------|      |------+------+------+------+------+------+------|
+ * |   =  | Esc  |   A  |   S  |   D  |   F  |   G  |      |   H  |   J  |   K  |   L  |   ;  |   "  | Home |
+ * |------+------+------+------+------+------+------|      |------+------+------+------+------+------+------|
+ * | Pg Up| Shift|   Z  |   X  |   C  |   V  |   B  |      |   N  |   M  |   ,  |   .  |   /  |Enter | End  |
+ * |------+------+------+------+------+------+------|      |------+------+------+------+------+------+------|
+ * | Pg Dn|Adjust| Ctrl | Alt  | GUI  |Lower |Space |      |Space |Raise | Left | Down |  Up  | Right|   \  |
+ * `------------------------------------------------'      `------------------------------------------------'
+ */
+  [_QWERTY] = LAYOUT_ortho_5x14(
+    KC_INS,  KC_GRV , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,     KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_BSLS, KC_LBRC,
+    KC_MINS, KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   ,     KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_BSPC, KC_RBRC,
+    KC_EQL,  CTL_ESC, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   ,     KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT, KC_HOME,
+    KC_PGUP, KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   ,     KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_ENT , KC_END ,
+    KC_PGDN, ADJUST , KC_LCTL, KC_LALT, KC_LGUI, LOWER  , KC_SPC ,     KC_BSPC , RAISE  , KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, KC_DEL
   ),
 
 /* Lower
