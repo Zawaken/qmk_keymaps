80e2fe44aea8a58df835ed0fab7851f35b03a18f
Zawaken <zawaken@outlook.com>
2022-01-27 18:17:20 +0100

    change help

--- flash.sh
+++ flash.sh
@@ -1,21 +1,36 @@
 #!/usr/bin/env bash
 
 
-show_help() {
-printf '    -h     display this menu and exit
+show_help() { # {{{
+printf 'Usage:
+
+    -h     display this menu and exit
     -b     build only mode, no flashing
     -v     verbose mode
     -p     build with podman instead of docker
+    -u     change QMK_USER
 
-Example: ./flash.sh -p -v preonic\n'
+Example: ./flash.sh -p -v preonic
+
+Keyboards:
+  planck
+  preonic
+  m60-a
+  viterbi
+  nyquist
+  vitamins_included
+  framework\n'
 }
-
+# }}}
+# variables # {{{
 build_only='false'
 QMK_FIRMWARE='qmk_firmware'
 QMK_USER='zawaken'
 eval LAST_ARG=\${$#}
 TARGET_LAYOUT=${LAST_ARG%/}
-
+verbose=0
+# }}}
+# argument handling {{{
 while true; do
   case $1 in
     -h|--help)
@@ -35,96 +50,77 @@ while true; do
       export RUNTIME="podman"
       #PODMAN=1
       ;;
+    -*)
+      show_help
+      printf "\nERR: Argument \"${1}\" does not exist\n"
+      exit 1
+      ;;
     *)
       break
       ;;
   esac
   shift
 done
-
+# }}}
 if test "${verbose}" -eq 1; then
   set -o xtrace
 fi
 
-if test "${TARGET_LAYOUT}" = "planck"; then
-  TARGET_KEYBOARD='planck'
-  MAKE_PREFIX='planck/rev6'
-  IMAGE_EXTENSION='bin'
-  MAKE_SUFFIX=':dfu-util-wait'
-elif test "${TARGET_LAYOUT}" = "preonic"; then
-  TARGET_KEYBOARD='preonic'
-  MAKE_PREFIX='preonic/rev3'
-  IMAGE_EXTENSION='bin'
-  MAKE_SUFFIX=":dfu-util-wait"
-elif test "${TARGET_LAYOUT}" = "m60-a"; then
-  TARGET_KEYBOARD='rama_works_m60_a'
-  MAKE_PREFIX='wilba_tech/rama_works_m60_a'
-  IMAGE_EXTENSION='hex'
-elif test "${TARGET_LAYOUT}" = "viterbi"; then
-  TARGET_KEYBOARD='keebio/viterbi'
-  MAKE_PREFIX='keebio/viterbi/rev2'
-  IMAGE_EXTENSION='hex'
-  MAKE_SUFFIX=':dfu'
-elif test "${TARGET_LAYOUT}" = "nyquist"; then
-  TARGET_KEYBOARD='keebio/nyquist'
-  MAKE_PREFIX='keebio/nyquist/rev3'
-  IMAGE_EXTENSION='hex'
-  MAKE_SUFFIX=':dfu'
-elif test "${TARGET_LAYOUT}" = "vitamins_included"; then
-  TARGET_KEYBOARD='vitamins_included'
-  MAKE_PREFIX='vitamins_included/rev2'
-  IMAGE_EXTENSION='hex'
-  MAKE_SUFFIX=':dfu'
+if test "${1}"; then
+  case "${TARGET_LAYOUT}" in
+    planck)
+      TARGET_KEYBOARD='planck'
+      MAKE_PREFIX='planck/rev6'
+      IMAGE_EXTENSION='bin'
+      MAKE_SUFFIX=':dfu-util-wait'
+      ;;
+    preonic)
+      TARGET_KEYBOARD='preonic'
+      MAKE_PREFIX='preonic/rev3'
+      IMAGE_EXTENSION='bin'
+      MAKE_SUFFIX=":dfu-util-wait"
+      ;;
+    m60-a)
+      TARGET_KEYBOARD='rama_works_m60_a'
+      MAKE_PREFIX='wilba_tech/rama_works_m60_a'
+      IMAGE_EXTENSION='hex'
+      ;;
+    viterbi)
+      TARGET_KEYBOARD='keebio/viterbi'
+      MAKE_PREFIX='keebio/viterbi/rev2'
+      IMAGE_EXTENSION='hex'
+      MAKE_SUFFIX=':dfu'
+      ;;
+    nyquist)
+      TARGET_KEYBOARD='keebio/nyquist'
+      MAKE_PREFIX='keebio/nyquist/rev3'
+      IMAGE_EXTENSION='hex'
+      MAKE_SUFFIX=':dfu'
+      ;;
+    vitamins_included)
+      TARGET_KEYBOARD='vitamins_included'
+      MAKE_PREFIX='vitamins_included/rev2'
+      IMAGE_EXTENSION='hex'
+      MAKE_SUFFIX=':dfu'
+      ;;
+    framework)
+      TARGET_KEYBOARD='framework'
+      MAKE_PREFIX='7c8/framework'
+      IMAGE_EXTENSION='hex'
+      MAKE_SUFFIX=':dfu'
+      ;;
+    *)
+      printf "Layout ${TARGET_LAYOUT} not found in the list, maybe you should add it?\nexiting\n"
+      exit 1
+      ;;
+  esac
 else
-  TARGET_KEYBOARD=$TARGET_LAYOUT
-  MAKE_PREFIX=$TARGET_LAYOUT
-  IMAGE_EXTENSION='hex'
+  show_help
+  printf "\nERR: No arguments supplied.\n"
+  exit
 fi
 
-#if test -z "${TARGET_LAYOUT}"; then
-#  case "${TARGET_LAYOUT}" in
-#    planck)
-#      TARGET_KEYBOARD='planck'
-#      MAKE_PREFIX='planck/rev6'
-#      IMAGE_EXTENSION='bin'
-#      MAKE_SUFFIX=':dfu-util-wait'
-#      ;;
-#    preonic)
-#      TARGET_KEYBOARD='preonic'
-#      MAKE_PREFIX='preonic/rev3'
-#      IMAGE_EXTENSION='bin'
-#      MAKE_SUFFIX=":dfu-util-wait"
-#      ;;
-#    m60-a)
-#      TARGET_KEYBOARD='rama_works_m60_a'
-#      MAKE_PREFIX='wilba_tech/rama_works_m60_a'
-#      IMAGE_EXTENSION='hex'
-#      ;;
-#    viterbi)
-#      TARGET_KEYBOARD='keebio/viterbi'
-#      MAKE_PREFIX='keebio/viterbi/rev2'
-#      IMAGE_EXTENSION='hex'
-#      MAKE_SUFFIX=':dfu'
-#      ;;
-#    nyquist)
-#      TARGET_KEYBOARD='keebio/nyquist'
-#      MAKE_PREFIX='keebio/nyquist/rev3'
-#      IMAGE_EXTENSION='hex'
-#      MAKE_SUFFIX=':dfu'
-#      ;;
-#    vitamins_included)
-#      TARGET_KEYBOARD='vitamins_included'
-#      MAKE_PREFIX='vitamins_included/rev2'
-#      IMAGE_EXTENSION='hex'
-#      MAKE_SUFFIX=':dfu'
-#      ;;
-#    *)
-#      printf "${TARGET_LAYOUT} not found in the list, maybe you should add it?\nexiting"
-#      exit 1
-#      ;;
-#  esac
-#fi
-
+# preonic_zawaken.bin
 BINARY_NAME="${TARGET_LAYOUT}_${QMK_USER}.${IMAGE_EXTENSION}"
 
 printf "\n===================================================
