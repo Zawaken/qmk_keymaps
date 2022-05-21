b1a191acdea6d7a96341c266fa67ed7224f98d72
Zawaken <zawaken@outlook.com>
2022-05-21 14:13:48 +0200

    make framework keymap work like the preonic one

--- framework/keymap.c
+++ framework/keymap.c
@@ -31,10 +31,11 @@ enum framework_keycodes {
     RAISE,
     ADJUST,
     ARROW,
+    SARCASM
 };
 // }}}
 // defines {{{
-#define CTL_ESC(MOD_LCTL, KC_ESC)
+#define CTL_ESC MT(MOD_LCTL, KC_ESC)
 #define ARROW MO(_ARROW)
 // }}}
 // keymaps {{{
@@ -45,35 +46,35 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,       KC_G,    KC_J,   KC_L,       KC_U,    KC_Y,    KC_SCLN,    KC_BSPC,
     CTL_ESC,  KC_A,    KC_R,    KC_S,    KC_T,       KC_D,    KC_H,   KC_N,       KC_E,    KC_I,    KC_O, KC_QUOT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,       KC_B,    KC_K,   KC_M,       KC_COMM, KC_DOT,  KC_SLSH, KC_SFTENT,
-    MO(_FN), KC_ALGR, KC_LALT, KC_LGUI, MO(_LOWER), KC_SPC,  KC_BSPC, MO(_RAISE), KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,
+    ARROW, KC_ALGR, KC_LALT, KC_LGUI, LOWER, KC_SPC,  KC_BSPC, RAISE, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,
     KC_AUDIO_VOL_DOWN, KC_AUDIO_VOL_UP
 ),
 // }}}
 // Lower {{{
 [_LOWER] = framework_via(
-    _______, _______, _______, _______, _______, _______, _______, _______,   _______, KC_MINS, KC_EQL,  _______,
-    KC_LEAD, KC_F1,   KC_F2,   KC_F3,   KC_F4,   _______, _______, _______,   _______, KC_LBRC, KC_RBRC, KC_BSLS,
-    KC_DEL,  KC_F5,   KC_F6,   KC_F7,   KC_F8,   _______, _______, _______,   _______, _______, _______, _______,
-    _______, KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______, _______,   _______, _______, _______, _______,
-    _______, _______, _______, _______, _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,
+    KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,
+    KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL,
+    KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE,
+    _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,S(KC_NUHS),S(KC_NUBS),KC_HOME, KC_END, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, KC_MPLY, KC_VOLD, KC_VOLU, KC_MNXT,
     C(S(KC_TAB)), C(KC_TAB)
 ),
 // }}}
 // Raise {{{
 [_RAISE] = framework_via(
-    _______, _______, _______, _______, _______,   _______, _______, _______, _______, _______, _______, _______,
-    _______, _______, _______, _______, _______,   _______, _______, _______, _______, _______, _______, _______,
-    _______, _______, _______, _______, _______,   _______, _______, _______, _______, _______, _______, _______,
-    _______, _______, _______, _______, _______,   _______, _______, _______, _______, _______, _______, _______,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
+    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
+    KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
+    _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NUHS, KC_NUBS, KC_PGUP, KC_PGDN, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, KC_MPLY, KC_VOLD, KC_VOLU, KC_MNXT,
     C(KC_LEFT), C(KC_RIGHT)
 ),
 // }}}
 // Adjust {{{
 [_ADJUST] = framework_via(
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______, RESET, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_DEL,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, SARCASM,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, EEP_RST, DEBUG,
     C(KC_Z), C(KC_Y)
@@ -82,7 +83,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // ARROW {{{
 [_ARROW] = framework_via(
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_DEL,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
@@ -121,7 +122,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
   if(sarcasm_flag) {
     if(record->event.pressed) {
-      if(ekycode != KC_SPC) {
+      if(keycode != KC_SPC) {
         register_code(KC_CAPS);
         unregister_code(KC_CAPS);
         caps_flag = !caps_flag;
@@ -137,6 +138,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         layer_off(_LOWER);
         update_tri_layer(_LOWER, _RAISE, _ADJUST);
       }
+      return false;
     case RAISE:
       if (record->event.pressed) {
         layer_on(_RAISE);
@@ -145,6 +147,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         layer_off(_RAISE);
         update_tri_layer(_LOWER, _RAISE, _ADJUST);
       }
+      return false;
     case SARCASM:
       if(record->event.pressed) {
         sarcasm_flag = !sarcasm_flag;
