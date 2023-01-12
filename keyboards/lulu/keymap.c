f9fcb2c5aaebd09de77aacc73f50d3f45b428532
Zawaken <zawaken@outlook.com>
2023-01-12 07:38:18 +0100

    update lulu to be able to flash with only one of the halves

--- keyboards/lulu/keymap.c
+++ keyboards/lulu/keymap.c
@@ -64,7 +64,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                   `----------------------------'           '------''--------------------'
  */
 [_LOWER] = LAYOUT(
-  _______, _______, _______, _______, _______, _______,                   _______, _______, _______,_______, _______, _______,
+  QK_BOOT, _______, _______, _______, _______, _______,                   _______, _______, _______,_______, _______, _______,
   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
   KC_TRNS, KC_TRNS, KC_LEFT,   KC_DOWN, KC_UP,  KC_RIGHT,                   KC_UNDS, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE,
   _______, _______, _______, _______, _______, _______, KC_MPLY,  _______, KC_F12, S(KC_NUHS), S(KC_NUBS), KC_HOME, KC_END, _______,
@@ -86,7 +86,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [_RAISE] = LAYOUT(
-  RGB_MOD, RGB_RMOD,RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD,                     RGB_TOG, _______, _______, _______, _______, _______,
+  RGB_MOD, RGB_RMOD,RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD,                     RGB_TOG, _______, _______, _______, _______, QK_BOOT,
   KC_TRNS,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                       _______, KC_MINS, KC_EQL, KC_LBRC,   KC_RBRC, KC_BSLS,
   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,   KC_MPLY, _______,  _______, KC_NUHS, KC_NUBS,  KC_PGUP, KC_PGDN, KC_BSLS,
