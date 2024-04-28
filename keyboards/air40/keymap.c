f20c44cc60ce4b980905a78a31904c96abc9ae6e
Zawaken <zawaken@outlook.com>
2024-04-28 11:55:17 +0200

    add F keys to lower layer blankslate

--- keyboards/air40/keymap.c
+++ keyboards/air40/keymap.c
@@ -1,4 +1,4 @@
-/* Copyright 2022 Dennis Kruyt (dennis@kruyt.org)
+/* Copyright 2022 Dennis Kruyt (dennis@kruyt.org) {{{
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -12,8 +12,7 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
+ */ // }}}
 #include QMK_KEYBOARD_H
 #include "common.h"
 
@@ -31,27 +30,53 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         ARROW,   KC_RALT, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_BSPC, RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
     ),
     [_ARROW] = LAYOUT_ortho_4x12(
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_DEL,
-        _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,
+        _______, RGB_TOG, RGB_HUI, RGB_HUD, RGB_VAI, _______, _______, _______, _______, _______, _______, KC_DEL,
+        _______, RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,
         _______, _______, _______, _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
     ),
     [_LOWER] = LAYOUT_ortho_4x12(
         KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,    KC_ASTR,    KC_LPRN, KC_RPRN, KC_BSPC,
         KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS,    KC_PLUS,    KC_LCBR, KC_RCBR, KC_PIPE,
         _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  S(KC_NUHS), S(KC_NUBS), KC_HOME, KC_END,  _______,
-        _______, _______, _______, _______, _______, _______, _______, _______,    KC_MNXT,    KC_VOLD, KC_VOLU, KC_MPLY
+        _______, _______, _______, _______, _______, _______, _______, _______,    _______,    _______, _______, _______
     ),
     [_RAISE] = LAYOUT_ortho_4x12(
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
         KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
         _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NUHS, KC_NUBS, KC_PGUP, KC_PGDN, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
+        _______, _______, _______, _______, _______, _______, _______, _______, KC_MPLY, KC_VOLD, KC_VOLU, KC_MNXT
     ),
     [_ADJUST] = LAYOUT_ortho_4x12(
-        QK_BOOT, RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, _______, _______, _______, _______, _______, _______, DB_TOGG,
-        _______, RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD, AG_NORM, AG_SWAP, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, QK_BOOT, DB_TOGG, RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, _______,
+        _______, _______, _______, _______, _______, AG_NORM, AG_SWAP, QWERTY,  COLEMAK, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_F20,  _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
     )
 };
+bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
+    rgb_matrix_set_color(41, 0, 0,0);
+    rgb_matrix_set_color(43, 0, 0,0);
+    rgb_matrix_set_color(45, 0, 0,0);
+    /* if (get_highest_layer(layer_state) > 0) { */
+    /*     uint8_t layer = get_highest_layer(layer_state); */
+    /**/
+    /*     for (uint8_t row = 0; row < MATRIX_ROWS; ++row) { */
+    /*         for (uint8_t col = 0; col < MATRIX_COLS; ++col) { */
+    /*             uint8_t index = g_led_config.matrix_co[row][col]; */
+    /**/
+    /*             if (index >= led_min && index < led_max && index != NO_LED && */
+    /*             keymap_key_to_keycode(layer, (keypos_t){col,row}) > KC_TRNS) { */
+    /*                 rgb_matrix_set_color(index, RGB_GREEN); */
+    /*             } */
+    /*         } */
+    /*     } */
+    /* } */
+    return false;
+    if (host_keyboard_led_state().caps_lock) {
+        RGB_MATRIX_INDICATOR_SET_COLOR(5, 255, 255, 255); // assuming caps lock is at led #5
+    } else {
+        RGB_MATRIX_INDICATOR_SET_COLOR(41, 0, 0, 0);
+    }
+    return false;
+}
