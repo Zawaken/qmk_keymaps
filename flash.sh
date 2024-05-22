71a322cac8897bc4275fd3dfdaf2b8d465d43f13
Zawaken <zawaken@outlook.com>
2024-05-22 21:33:13 +0200

    update flash script

--- flash.sh
+++ flash.sh
@@ -1,55 +1,22 @@
 #!/usr/bin/env bash
 # variables # {{{
-build_only='false'
 FW_FOLDER='qmk_firmware'
-# BUILD_SCRIPT='./container_build.sh'
-BUILD_SCRIPT='./util/docker_build.sh'
-QMK_USER="${USER}"
 eval LAST_ARG=\${$#}
 TARGET_LAYOUT=${LAST_ARG%/}
-verbose=0
-RUNTIME="local"
 # }}}
-show_help() { # {{{
-printf 'Usage:
 
-    -h     display this menu and exit
-    -b     build only mode, no flashing
-    -v     verbose mode
-    -p     build with podman
-    -d     build with docker
-    -l     build locally with qmk cli (default)
-
-Example: ./flash.sh -p -v preonic
-
-Keyboards:
-  planck
-  preonic
-  m60-a
-  viterbi
-  nyquist
-  vitamins_included
-  framework
-  lulu\n'
-}
-# }}}
 # argument handling {{{
+RUNTIME="podman" QMK_USER="${USER}" BUILD_SCRIPT="./util/docker_build.sh" BUILD_ONLY=false
 while true; do
   case $1 in
-    -h|--help)
-      show_help
-      exit
-      ;;
     -v|--verbose) verbose=1;;
     -u|--user) QMK_USER="${2}";;
-    -b|--build_only) build_only=true;;
+    -b|--build_only) BUILD_ONLY=true;;
     -p|--podman) RUNTIME="podman";;
     -d|--docker) RUNTIME="docker";;
     -l|--local) RUNTIME="local";;
-    -u|--user) QMK_USER="${2}";;
     -*)
-      show_help
-      printf "\nERR: Argument \"${1}\" does not exist\n"
+      printf "ERR: Argument \"%s\" does not exist\n" "${1}"
       exit 1
       ;;
     *)
@@ -62,70 +29,71 @@ if test "${verbose}" -eq 1; then
   set -o xtrace
 fi
 # }}}
+
 if test "${1}"; then # {{{
   case "${TARGET_LAYOUT}" in
     planck)
-      TARGET_KEYBOARD='planck'
+      TARGET_KBD='planck'
       MAKE_PREFIX='planck/rev6'
       IMAGE_EXTENSION='bin'
       MAKE_SUFFIX='dfu-util'
       ;;
     preonic)
-      TARGET_KEYBOARD='preonic'
+      TARGET_KBD='preonic'
       MAKE_PREFIX='preonic/rev3'
       IMAGE_EXTENSION='bin'
       MAKE_SUFFIX="dfu-util"
       ;;
     m60-a)
-      TARGET_KEYBOARD='rama_works_m60_a'
+      TARGET_KBD='rama_works_m60_a'
       MAKE_PREFIX='wilba_tech/rama_works_m60_a'
       IMAGE_EXTENSION='hex'
       ;;
     viterbi)
-      TARGET_KEYBOARD='keebio/viterbi'
+      TARGET_KBD='keebio/viterbi'
       MAKE_PREFIX='keebio/viterbi/rev2'
       IMAGE_EXTENSION='hex'
       MAKE_SUFFIX='dfu'
       ;;
     nyquist)
-      TARGET_KEYBOARD='keebio/nyquist'
+      TARGET_KBD='keebio/nyquist'
       MAKE_PREFIX='keebio/nyquist/rev3'
       IMAGE_EXTENSION='hex'
       MAKE_SUFFIX='dfu'
       ;;
     lpvi)
-      TARGET_KEYBOARD='vitamins_included'
+      TARGET_KBD='vitamins_included'
       MAKE_PREFIX='vitamins_included/rev2'
       IMAGE_EXTENSION='hex'
       MAKE_SUFFIX='dfu'
       ;;
     framework)
-      TARGET_KEYBOARD='7c8/framework'
-      MAKE_PREFIX='7c8/framework'
+      TARGET_KBD='7c8/framework'
+      MAKE_PREFIX="$TARGET_KBD"
       IMAGE_EXTENSION='bin'
       MAKE_SUFFIX='flash'
       ;;
     framework-via)
-      TARGET_KEYBOARD='7c8/framework'
-      MAKE_PREFIX='7c8/framework'
+      TARGET_KBD='7c8/framework'
+      MAKE_PREFIX="$TARGET_KBD"
       IMAGE_EXTENSION='bin'
       MAKE_SUFFIX='flash'
       ;;
     lulu)
-      TARGET_KEYBOARD='boardsource/lulu'
-      MAKE_PREFIX='boardsource/lulu/rp2040'
+      TARGET_KBD='boardsource/lulu'
+      MAKE_PREFIX="$TARGET_KBD/rp2040"
       IMAGE_EXTENSION='uf2'
       MAKE_SUFFIX='flash'
       ;;
     conundrum)
-      TARGET_KEYBOARD='thock/conundrum'
-      MAKE_PREFIX='thock/conundrum'
+      TARGET_KBD='thock/conundrum'
+      MAKE_PREFIX="$TARGET_KBD"
       IMAGE_EXTENSION='uf2'
       MAKE_SUFFIX='flash'
       ;;
     air40)
-      TARGET_KEYBOARD='ymdk/ymd40/air40'
-      MAKE_PREFIX="$TARGET_KEYBOARD"
+      TARGET_KBD='ymdk/ymd40/air40'
+      MAKE_PREFIX="$TARGET_KBD"
       IMAGE_EXTENSION='hex'
       MAKE_SUFFIX='flash'
       ;;
@@ -134,22 +102,21 @@ if test "${1}"; then # {{{
       exit 0
       ;;
     *)
-      printf "Layout ${TARGET_LAYOUT} not found in the list, maybe you should add it?\nexiting\n"
+      printf "Layout %s not found in the list, maybe you should add it?\nexiting\n" "${TARGET_LAYOUT}"
       exit 1
       ;;
   esac
 else
-  show_help
   printf "\nERR: No arguments supplied.\n"
   exit
 fi #}}}
 
-get_make_command() {
+get_make_command() { # {{{
   MAKE_COMMAND="${MAKE_PREFIX}:${QMK_USER}"
-  if ! $build_only; then
+  if ! $BUILD_ONLY; then
     MAKE_COMMAND="${MAKE_COMMAND}:${MAKE_SUFFIX}"
   fi
-}
+} # }}}
 
 fw_lock() { # {{{
   GIT_DIR="git --git-dir ${FW_FOLDER}/.git"
@@ -157,44 +124,44 @@ fw_lock() { # {{{
     git clone https://github.com/qmk/qmk_firmware.git ${FW_FOLDER}
   fi
   git --git-dir ${FW_FOLDER}/.git config advice.detachedHead false
-  if test -f keyboards/${TARGET_LAYOUT}/fw.lock; then
-    FW_LOCK_HASH=$(cat keyboards/${TARGET_LAYOUT}/fw.lock)
+  if test -f "keyboards/${TARGET_LAYOUT}/fw.lock"; then
+    FW_LOCK_HASH=$(cat "keyboards/${TARGET_LAYOUT}/fw.lock")
     if $(printf '%s\n' "$(git --git-dir ${FW_FOLDER}/.git rev-parse HEAD)") -eq FW_LOCK_HASH; then
       git --git-dir ${FW_FOLDER} checkout "$FW_LOCK_HASH"
     fi
   else
     git checkout master
     git pull
-    git rev-parse HEAD > keyboards/${TARGET_LAYOUT}/fw.lock
+    git rev-parse HEAD > "keyboards/${TARGET_LAYOUT}/fw.lock"
   fi
 } # }}}
 
-conundrum_build() {
-  FW_FOLDER ='thockqmk'
+conundrum_build() { # {{{
+  FW_FOLDER='thockqmk'
   cp -r "${KEYMAP_DIR}" "${FW_KEYMAP_DIR}"
   cd thockqmk
   RUNTIME=${RUNTIME} ./build-conundrum.sh "${QMK_USER}:uf2"
 }
 get_make_command
 
-if [ $TARGET_LAYOUT == "conundrum" ]; then
+if [ "$TARGET_LAYOUT" == "conundrum" ]; then
   FW_FOLDER="thockqmk"
   BUILD_SCRIPT="./build-conundrum.sh"
   MAKE_COMMAND="${QMK_USER}:uf2"
   export keymap="${QMK_USER}:uf2"
   # conundrum_build
   # exit 0
-fi
+fi # }}}
 
 # preonic_zawaken.bin
-BINARY_NAME="$(printf "${MAKE_PREFIX}" | sed -e 's/\//_/g')_${QMK_USER}.${IMAGE_EXTENSION}"
+BINARY_NAME="$(printf "%s" "${MAKE_PREFIX}" | sed -e 's/\//_/g')_${QMK_USER}.${IMAGE_EXTENSION}"
 KEYMAP_DIR="keyboards/${TARGET_LAYOUT}"
-FW_KEYMAPS_DIR="${FW_FOLDER}/keyboards/${TARGET_KEYBOARD}/keymaps"
+FW_KEYMAPS_DIR="${FW_FOLDER}/keyboards/${TARGET_KBD}/keymaps"
 FW_KEYMAP_DIR="${FW_KEYMAPS_DIR}/${QMK_USER}"
 FW_COMMON_DIR="${FW_FOLDER}/users/${QMK_USER}"
 
 printf "\n===================================================
-KEYBOARD:         \"${TARGET_KEYBOARD}\"
+KEYBOARD:         \"${TARGET_KBD}\"
 LAYOUT:           \"${TARGET_LAYOUT}\"
 BINARY:           \"${BINARY_NAME}\"
 FW_FOLDER:        \"${FW_FOLDER}\"
@@ -205,26 +172,25 @@ MAKE_COMMAND:     \"${MAKE_COMMAND}\"
 ===================================================\n"
 
 main () { # {{{
-make --directory="${FW_FOLDER}" git-submodule >/dev/null 2>&1 # Submodule initialization
-rm -rf  "${FW_FOLDER:?}/.build" \
-        "${FW_KEYMAP_DIR:?}" \
-        "${FW_COMMON_DIR:?}"
-mkdir -p "${FW_KEYMAPS_DIR}" # Create TARGET_KEYBOARD/keymaps/ folder
-mkdir -p "${FW_FOLDER}/.build"
-cp -r "${KEYMAP_DIR}" "${FW_KEYMAP_DIR}"
-cp -r common "${FW_COMMON_DIR}"
-# rsync -avh ${KEYMAP_DIR}/{config.h,keymap.c,rules.mk} "${FW_KEYMAP_DIR}" >/dev/null 2>&1
-# rsync -avh --delete common/ ${FW_FOLDER}/users/${QMK_USER}/ >/dev/null 2>&1 # Copy everything in the common directory into users/QMK_USER
-rm -rf "${FW_FOLDER:?}/${BINARY_NAME}"
+  make --directory="${FW_FOLDER}" git-submodule >/dev/null 2>&1 # Submodule initialization
+  rm -rf  "${FW_FOLDER:?}/.build" \
+          "${FW_KEYMAP_DIR:?}" \
+          "${FW_COMMON_DIR:?}"
+  mkdir -p "${FW_KEYMAPS_DIR}" # Create TARGET_KBD/keymaps/ folder
+  mkdir -p "${FW_FOLDER}/.build"
+  cp -r "${KEYMAP_DIR}" "${FW_KEYMAP_DIR}"
+  cp -r common "${FW_COMMON_DIR}"
+  # rsync -avh ${KEYMAP_DIR}/{config.h,keymap.c,rules.mk} "${FW_KEYMAP_DIR}" >/dev/null 2>&1
+  # rsync -avh --delete common/ ${FW_FOLDER}/users/${QMK_USER}/ >/dev/null 2>&1 # Copy everything in the common directory into users/QMK_USER
+  rm -rf "${FW_FOLDER:?}/${BINARY_NAME}"
 
-# Build/flash
-# cd "${FW_FOLDER}"
-case $RUNTIME in
-  # TODO: make the flash script work with util/docker_build.sh from qmk_firmware
-  # podman|docker) RUNTIME="${RUNTIME}" ./util/docker_build.sh ${MAKE_COMMAND};;
-  podman|docker)cd "${FW_FOLDER}";  RUNTIME="${RUNTIME}" "${BUILD_SCRIPT}" ${MAKE_COMMAND};;
-  local) ;;
-esac
+  cd "${FW_FOLDER}"
+  # Build/flash
+  # cd "${FW_FOLDER}"
+  case $RUNTIME in
+    podman|docker) RUNTIME="${RUNTIME}" "${BUILD_SCRIPT}" "${MAKE_COMMAND}";;
+    local) ;;
+  esac
 } # }}}
 
 main
